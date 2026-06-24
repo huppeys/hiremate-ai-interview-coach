cd51281e92c83f8a827b13d6beb1997d8db6e065
Kay Bee <kaceybanks@my.unt.edu>
2026-06-23 21:24:00 -0500

    Added supabase authentication and database

---
+++ frontend/src/authority.js
@@ -0,0 +1,36 @@
+import { supabase } from "./supabase";
+
+export async function register(email, password) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function login(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw error;
+}
+
+export async function resetPassword(email) {
+  const { error } =
+    await supabase.auth.resetPasswordForEmail(email);
+
+  if (error) throw error;
+}
