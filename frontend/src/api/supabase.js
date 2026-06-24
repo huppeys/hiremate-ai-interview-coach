5d2d0d9ed1ec8b33e5d69ed0aabdde5ae56ae254
KaceyBanks <KaceyBanks@my.unt.edu>
2026-06-24 00:58:11 -0500

    Delete frontend/src/api/supabase.js

--- frontend/src/api/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://uwzqamnvrgefwvruahrb.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3enFhbW52cmdlZnd2cnVhaHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMjYyNjksImV4cCI6MjA5NjgwMjI2OX0.UhRS0Z8ddxdMHytf02SVt9E-OQfTeHI8EdxuyvEiBe0";
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
