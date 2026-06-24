1daf3e8d5c2594f33deb9f722cdab23a4b590d24
KaceyBanks <KaceyBanks@my.unt.edu>
2026-06-23 23:09:17 -0500

    Update supabase.js

--- frontend/src/api/supabase.js
+++ frontend/src/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseUrl = "https://uwzqamnvrgefwvruahrb.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3enFhbW52cmdlZnd2cnVhaHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMjYyNjksImV4cCI6MjA5NjgwMjI2OX0.UhRS0Z8ddxdMHytf02SVt9E-OQfTeHI8EdxuyvEiBe0";
 
 export const supabase = createClient(
   supabaseUrl,
